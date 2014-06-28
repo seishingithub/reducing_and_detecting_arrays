3cd78bdfe915515a174058ca051746edaa40f959
Peggy Griffin <seishingithub@gmail.com>
2014-06-28 17:17:46 -0600

    Initial commit

---
+++ spec/students_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+require './lib/students'
+
+describe Students do
+  students = Students.new
+
+  it 'can return all students' do
+    pending
+  end
+
+  it 'can calculate the average age of the class' do
+    pending
+      end
+
+  it 'can return a string of student names' do
+    pending
+  end
+
+  it 'can find a student older than a certain age' do
+    pending
+  end
+
+  it 'can find out if there are any old enough to drink' do
+    pending
+  end
+
+  it 'can find a specific student' do
+    pending
+  end
+
+  it 'can determine if a specific student is present' do
+    pending
+  end
+
+  it 'can group students by age' do
+    pending
+  end
+end
